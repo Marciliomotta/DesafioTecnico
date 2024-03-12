e1f0b8d8b543e33bfe45a3f3027a3e035f561bad
MarcílioMotta <marciliolima597@gmail.com>
2024-03-12 15:32:58 -0300

    -- Criptografia

---
+++ Utils/Criptografia.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class Criptografia
+    {
+        public static string EsconderParametros(string _cadeia)
+        {
+            try
+            {
+                char[] charArray = _cadeia.ToCharArray();
+                byte[] byteArray = new byte[charArray.Length];
+                string texto = string.Empty;
+                for (int i = 0; i < charArray.Length; i++)
+                    byteArray[i] = Convert.ToByte(charArray[i]);
+
+                return Convert.ToBase64String(byteArray);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string MostrarParametros(string _cadeia)
+        {
+            try
+            {
+                byte[] valorByte = Convert.FromBase64String(_cadeia);
+                char[] charArray = new char[valorByte.Length];
+                for (int i = 0; i < valorByte.Length; i++)
+                    charArray[i] = Convert.ToChar(valorByte[i]);
+
+                return new string(charArray);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
